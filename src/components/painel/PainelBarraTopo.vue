08eb868dcad5801b52f783de3e9b0af4719697bd
Carlos Roberto <crpu4a@gmail.com>
2021-07-20 16:49:14 -0300

    feat: criação da barra de topo contendo componente usuario

---
+++ src/components/painel/PainelBarraTopo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="barra__conteudo">
+    <div>
+      <!-- Espaço útil da barra -->
+    </div>
+    <painel-usuario
+      fotoUsuario="https://gravatar.com/avatar/9a82ba52769cbc22951a7cf516124307?s=50"
+      nomeUsuario="Carlos Roberto da Silva Júnior"
+      cargoUsuario="Desenvolvedor Front-End"
+    />
+  </div>
+</template>
+
+<script>
+import PainelUsuario from "./PainelUsuario.vue";
+export default {
+  components: { PainelUsuario },
+};
+</script>
+
+<style lang="stylus" scoped>
+.barra__conteudo
+  display grid;
+  grid-template-columns auto 300px
+  box-sizing border-box
+  max-height 85px
+  padding 15px 30px
+  margin-bottom: 10px
+  background var(--cor-background)
+  box-shadow var(--sombra-padrao)
+</style>
---
+++ src/components/painel/PainelUsuario.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="usuario">
+    <img class="usuario__foto" :src="fotoUsuario" :alt="nomeUsuario" />
+    <div class="usuario__informacoes">
+      <h4 class="usuario__nome">
+        {{ nomeUsuario }}
+      </h4>
+      <p class="usuario__cargo">
+        {{ cargoUsuario }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nomeUsuario: String,
+    cargoUsuario: String,
+    fotoUsuario: String,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.usuario
+  box-sizing border-box
+  grid-template-columns 50px auto
+  display grid;
+
+.usuario__foto
+  height 100%
+  border-radius 50%
+
+.usuario__informacoes
+  box-sizing border-box
+  padding: 0px 15px
+  align-self center
+  text-align left
+
+.usuario__nome,
+.usuario__cargo
+  padding 0px
+  margin 0px
+
+.usuario__nome
+  font-weight 600;
+  font-size 15px;
+  line-height 16px;
+
+.usuario__cargo
+  font-weight 600;
+  font-size 12px;
+  line-height 13px;
+  color: var(--cor-principal)
+</style>
