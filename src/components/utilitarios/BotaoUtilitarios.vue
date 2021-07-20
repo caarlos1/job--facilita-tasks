2406143ba9dc174a294cad3df4cdb7fe6503bfef
Carlos Roberto <crpu4a@gmail.com>
2021-07-19 23:15:35 -0300

    feat: criação dos dois componentes de utilitários: Botao e Input

---
+++ src/components/utilitarios/BotaoUtilitarios.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="botao__bloco">
+    <button type="submit" class="botao" :class="classesDinamicas">
+      {{ botao || "Enviar" }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.modificarCorBotao(this.cor);
+  },
+  props: {
+    botao: String,
+    responsivo: Boolean,
+    cor: String,
+  },
+
+  data() {
+    return {
+      classesDinamicas: {
+        "botao--100": this.responsivo,
+      },
+    };
+  },
+
+  methods: {
+    modificarCorBotao(cor) {
+      this.classesDinamicas[cor] = true;
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.botao__bloco
+  margin 20px 0px
+
+.botao
+  transition 0.1s linear
+  background var(--cor-principal)
+  padding 18px
+  border none
+  border-radius 5px
+  color: #FFF
+  font-weight 700
+  font-size 16px
+
+.botao:hover
+.botao:active
+  background var(--cor-principal-hover)
+
+.botao--100
+  width 100%
+
+.sucesso
+  background var(--cor-sucesso)
+
+.sucesso:hover
+.sucesso:active
+  background var(--cor-sucesso-hover)
+</style>
---
+++ src/components/utilitarios/InputUtilitarios.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="input__bloco">
+    <label class="input__label" :for="'input_' + nome">{{ descricao }}</label>
+    <input
+      :id="'input_' + nome"
+      class="input__input"
+      :type="tipo || 'text'"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    descricao: String,
+    nome: String,
+    placeholder: String,
+    tipo: String,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.input__bloco
+  margin-top 20px
+
+.input__label
+  margin-bottom 10px
+  font-weight 600
+  font-size 14px
+  color var(--cor-borda-input)
+
+.input__input
+  height 55px
+  width 100%
+  margin-top 8px
+  color var(--cor-borda-input)
+  background var(--cor-background)
+  border 2px solid #BFDAEB
+  box-sizing border-box
+  border-radius 5px
+  text-indent 20px
+
+.input__input:focus
+  outline-color var(--cor-principal)
+</style>
