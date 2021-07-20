63109d2906ea57eb5e38dc04766d2eb042a8f435
Carlos Roberto <crpu4a@gmail.com>
2021-07-19 23:16:24 -0300

    feat: importação dos arquivos de estilo em App.vue e refatoração

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Página de Acesso</router-link> |
-      <router-link to="/painel">Painel Principal</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<script>
+// Arquivos CSS de Suporte
+import "normalize.css";
+
+// Aqruivso Stylus
+import "./assets/css/root.styl";
+import "./assets/css/fonts.styl";
+import "./assets/css/style.styl";
+
+export default {
+  setup() {},
+};
+</script>
