0bfd09f00010483da92cb1ed15b68cb623181d64
Carlos Roberto <crpu4a@gmail.com>
2021-07-21 18:40:52 -0300

    refactor: modificação para as funções de Util participarem de uma classe com o mesmo nome

--- src/util/index.js
+++ src/util/index.js
@@ -1,11 +1,4 @@
-export function salvarTarefasCache(state) {
-  const tarefas = JSON.stringify(state.tarefas);
-  localStorage.setItem("listaTarefasUsuario", tarefas);
-}
-export function limparTarefasCache() {
-  localStorage.setItem("listaTarefasUsuario", "");
-}
-
-export function buscarTarefasCache() {
-  return localStorage.getItem("listaTarefasUsuario");
+export class Util {
+  static toJSON = (dados) => JSON.parse(dados);
+  static isObjetoVazio = (obj) => Object.values(obj).length;
 }
