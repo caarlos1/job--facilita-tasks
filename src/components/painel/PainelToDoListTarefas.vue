e52f733dafc4ddc271a14df3927b7d60420c48c5
Carlos Roberto <crpu4a@gmail.com>
2021-07-21 08:36:44 -0300

    refactor: refatoração de PainelToDoListTarefas para retirar data de tarefas do seu escopo

--- src/components/painel/PainelToDoListTarefas.vue
+++ src/components/painel/PainelToDoListTarefas.vue
@@ -1,30 +1,11 @@
 <template>
-  <painel-to-do-list-tarefa-loop :tarefas="tarefas" />
+  <painel-to-do-list-tarefa-loop />
 </template>
 
 <script>
 import PainelToDoListTarefaLoop from "./PainelToDoListTarefaLoop.vue";
+
 export default {
   components: { PainelToDoListTarefaLoop },
-  data() {
-    return {
-      tarefas: [
-        {
-          id: 1,
-          titulo: "Criar projeto Vue.js",
-          descricao: "Fazer bem feito e bonito.",
-          finalizada: false,
-        },
-        {
-          id: 2,
-          titulo: "Criar projeto Vue.js",
-          descricao: "Fazer bem feito e bonito.",
-          finalizada: true,
-        },
-      ],
-    };
-  },
 };
 </script>
-
-<style lang="stylus" scoped></style>
