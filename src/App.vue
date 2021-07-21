ec121270edc7c1c38ab508e8d59372017b477f93
Carlos Roberto <crpu4a@gmail.com>
2021-07-21 08:25:09 -0300

    feat: adição de lógica de carregamento do cache das tarefas no mounted de App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,22 @@ import "normalize.css";
 import "./assets/css/root.styl";
 import "./assets/css/fonts.styl";
 import "./assets/css/style.styl";
+
 import "@fortawesome/fontawesome-free/css/all.css";
 
+import { mapActions } from "vuex";
+import { buscarTarefasCache, limparTarefasCache } from "./util";
+
 export default {
-  setup() {},
+  mounted() {
+    const tarefas = buscarTarefasCache();
+    if (tarefas) {
+      if (tarefas.length > 0) this.atualizarTarefas(tarefas);
+      else limparTarefasCache();
+    }
+  },
+  methods: {
+    ...mapActions("toDoList", ["atualizarTarefas"]),
+  },
 };
 </script>
