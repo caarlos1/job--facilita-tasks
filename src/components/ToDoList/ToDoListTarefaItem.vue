0863c20c55eb58baa795d0ef79fd3f3247ad99b1
Carlos Roberto <crpu4a@gmail.com>
2021-07-22 15:27:55 -0300

    refactor: separação de ToDoListTarefaItem de ToDoListTarefas

---
+++ src/components/ToDoList/ToDoListTarefaItem.vue
@@ -0,0 +1,98 @@
+<template>
+  <div
+    :class="{ 'tarefa--concluida': verificarEstadoTarefa(tarefa) }"
+    class="tarefa__bloco"
+  >
+    <button
+      class="tarefa__estado"
+      @click="mudarEstadoTarefa(tarefa.id)"
+    ></button>
+
+    <h4 class="tarefa__titulo">
+      {{ tarefa.titulo }}
+    </h4>
+
+    <div class="tarefa__tipo">
+      <span class="tarefa__tags" :class="classPorValor(tarefa.tipo.valor)">
+        {{ tarefa.tipo.tag || "" }}
+      </span>
+    </div>
+
+    <i class="fas fa-ellipsis-v tarefa__acoes"></i>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import { UtilTarefas } from "../../util";
+
+export default {
+  props: {
+    tarefa: Object,
+  },
+  methods: {
+    ...mapActions("toDoList", ["mudarEstadoTarefa", "limparTarefas"]),
+    classPorValor: (valor) => UtilTarefas.definirClassPorValor(valor),
+    verificarEstadoTarefa: (tarefa) => tarefa.concluida,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.tarefa__bloco
+  display grid;
+  grid-template-columns 35px auto 110px 20px
+  background var(--cor-background)
+  box-shadow var(--sombra-padrao)
+  border-radius 5px
+  padding 20px 30px
+
+.tarefa__titulo
+  padding 0 12px
+  margin 0
+  align-self center
+  font-weight 600
+  font-size 15px
+  line-height 16px
+
+.tarefa__estado
+  height 35px
+  width 35px
+  background #E2EEF5
+  border-radius 5px
+  border none
+  transition var(--transicao-padrao)
+  cursor pointer
+
+.tarefa__tags
+  padding 4px 10px
+  color var(--cor-texto-claro)
+  border-radius 20px
+  text-align center
+  font-weight: bold;
+  font-size: 11px;
+  line-height: 12px;
+
+.tarefa__tipo
+  align-self center
+  text-align right
+
+.tarefa__acoes
+  padding 0px 0 0 20px
+  align-self center
+  font-size 18px
+  color var(--cor-borda-input)
+
+.tarefa--concluida
+  transition var(--transicao-padrao)
+  opacity 0.5
+
+  .tarefa__estado
+    background: url(../../assets/icon/concluido.svg)
+    background-color: var(--cor-sucesso)
+    background-size 20px
+    background-position center center
+    background-repeat no-repeat
+  .tarefa__titulo
+    text-decoration line-through
+</style>
--- src/components/ToDoList/ToDoListTarefas.vue
+++ src/components/ToDoList/ToDoListTarefas.vue
@@ -1,28 +1,8 @@
 <template>
   <div class="tarefas__area">
-    <div
-      v-for="tarefa in listaPorTipo"
-      :key="tarefa.id"
-      class="tarefa__loop"
-      :class="{ 'tarefa--finalizada': tarefa.finalizada }"
-    >
-      <div class="tarefa__bloco">
-        <button
-          class="tarefa__estado"
-          @click="mudarEstadoTarefa(tarefa.id)"
-        ></button>
-
-        <h4 class="tarefa__titulo">{{ tarefa.titulo }}</h4>
-
-        <div class="tarefa__importancia">
-          <span
-            class="tarefa__tags"
-            :class="tagPorTipo(tarefa.importancia.tipo)"
-          >
-            {{ tarefa.importancia.texto || "" }}
-          </span>
-        </div>
-        <i class="fas fa-ellipsis-v tarefa__acoes"></i>
+    <div class="tarefa__lista">
+      <div v-for="tarefa in obterLista()" :key="tarefa.id" class="tarefa__item">
+        <to-do-list-tarefa-item :tarefa="tarefa" />
       </div>
     </div>
 
@@ -33,14 +13,18 @@
 
 <script>
 import { mapActions, mapGetters } from "vuex";
-import { definirListaPorTipo, definirTagPorTipo } from "./ToDoListUtil";
-import { Util } from "../../util";
+import { Util, UtilTarefas } from "../../util";
+import ToDoListTarefaItem from "./ToDoListTarefaItem.vue";
 
 export default {
+  components: { ToDoListTarefaItem },
   methods: {
-    ...mapActions("toDoList", ["mudarEstadoTarefa", "limparTarefas"]),
+    ...mapActions("toDoList", ["limparTarefas"]),
 
-    tagPorTipo: (tipo) => definirTagPorTipo(tipo),
+    obterLista() {
+      const tipo = this.buscarTipoDeTarefasPelaURL;
+      return UtilTarefas.definirListaPorTipo(tipo, this);
+    },
   },
 
   computed: {
@@ -52,11 +36,6 @@ export default {
       "listaDeTarefasFinalizadas",
     ]),
 
-    listaPorTipo() {
-      const tipo = this.buscarTipoDeTarefasPelaURL;
-      return definirListaPorTipo(tipo, this);
-    },
-
     buscarTipoDeTarefasPelaURL() {
       const parametros = this.$route.params;
       return Util.isObjetoVazio(parametros) > 0 ? parametros.tipo : "";
@@ -66,62 +45,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.tarefa__loop
+.tarefa__item
   margin 10px 0px
   transition var(--transicao-padrao)
-
-.tarefa__bloco
-  display grid;
-  grid-template-columns 35px auto 110px 20px
-  background var(--cor-background)
-  box-shadow var(--sombra-padrao)
-  border-radius 5px
-  padding 20px 30px
-
-.tarefa__titulo
-  padding 0 12px
-  margin 0
-  align-self center
-  font-weight 600
-  font-size 15px
-  line-height 16px
-
-.tarefa__estado
-  height 35px
-  width 35px
-  background #E2EEF5
-  border-radius 5px
-  border none
-  transition var(--transicao-padrao)
-
-.tarefa--finalizada
-  transition var(--transicao-padrao)
-  opacity 0.5
-  .tarefa__estado
-    background: url(../../assets/icon/concluido.svg)
-    background-color: var(--cor-sucesso)
-    background-size 20px
-    background-position center center
-    background-repeat no-repeat
-  .tarefa__titulo
-    text-decoration line-through
-
-.tarefa__tags
-  padding 4px 10px
-  color var(--cor-texto-claro)
-  border-radius 20px
-  text-align center
-  font-weight: bold;
-  font-size: 11px;
-  line-height: 12px;
-
-.tarefa__importancia
-  align-self center
-  text-align right
-
-.tarefa__acoes
-  padding 0px 0 0 20px
-  align-self center
-  font-size 18px
-  color var(--cor-borda-input)
 </style>
