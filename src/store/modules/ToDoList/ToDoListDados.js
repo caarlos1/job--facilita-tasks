566c8eb7ab6765de0cd22f6c7be3a005747a9fe2
Carlos Roberto <crpu4a@gmail.com>
2021-07-22 15:56:54 -0300

    refactor: adição de mais tarefas automaticas ao ToDoListDados

--- src/store/modules/ToDoList/ToDoListDados.js
+++ src/store/modules/ToDoList/ToDoListDados.js
@@ -1,19 +1,9 @@
 export const tarefas = [
   {
     id: 1,
-    titulo: "Criar projeto Vue.js",
-    descricao: "Fazer bem feito e bonito.",
-    concluida: false,
-    tipo: {
-      valor: 1,
-      tag: "Normal",
-    },
-  },
-  {
-    id: 2,
-    titulo: "Começar a estudar Vue.js",
-    descricao: "Fazer bem feito e bonito.",
-    concluida: false,
+    titulo: "Aprender Vue.js",
+    descricao: "É um framework muito importante.",
+    concluida: true,
     tipo: {
       valor: 2,
       tag: "Importante",
@@ -21,7 +11,37 @@ export const tarefas = [
   },
   {
     id: 3,
-    titulo: "Criar projeto Vue.js",
+    titulo: "Limpar a areia do gato!",
+    descricao: "Ou ele vai te deixar um presentinho...",
+    concluida: false,
+    tipo: {
+      valor: 3,
+      tag: "Urgente",
+    },
+  },
+  {
+    id: 2,
+    titulo: "Começar o Projeto Facilita Tasks.",
+    descricao: "Atenção para o Vuex e Router.",
+    concluida: true,
+    tipo: {
+      valor: 2,
+      tag: "Importante",
+    },
+  },
+  {
+    id: 4,
+    titulo: "Tomar banho...",
+    descricao: "É importante, ninguém quer mais chegar perto de você...",
+    concluida: false,
+    tipo: {
+      valor: 1,
+      tag: "Normal",
+    },
+  },
+  {
+    id: 5,
+    titulo: "Comprar café!!!",
     descricao: "Fazer bem feito e bonito.",
     concluida: false,
     tipo: {
