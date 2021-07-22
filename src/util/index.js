95f1c3a3ee72b7c074a3e97dd283193b27293ccd
Carlos Roberto <crpu4a@gmail.com>
2021-07-22 15:30:34 -0300

    refactor: migração de funções de ToDoListUtil para classe UtilTarefas em ./util

--- src/util/index.js
+++ src/util/index.js
@@ -2,3 +2,42 @@ export class Util {
   static toJSON = (dados) => JSON.parse(dados);
   static isObjetoVazio = (obj) => Object.values(obj).length;
 }
+
+export class UtilTarefas {
+  static definirClassPorValor = (valor) => {
+    switch (valor) {
+      case 2:
+        return "alerta";
+      case 3:
+        return "perigo";
+      default:
+        return "primaria";
+    }
+  };
+
+  static definirTagPorValor = (valor) => {
+    switch (valor) {
+      case 2:
+        return "Importante";
+      case 3:
+        return "Urgente";
+      default:
+        return "Normal";
+    }
+  };
+
+  static definirListaPorTipo = (tipo, objeto) => {
+    switch (tipo) {
+      case "urgentes":
+        return objeto.listaDeTarefasUrgentes;
+      case "importantes":
+        return objeto.listaDeTarefasImportantes;
+      case "outras":
+        return objeto.listaDeTarefasOutras;
+      case "finalizadas":
+        return objeto.listaDeTarefasFinalizadas;
+      default:
+        return objeto.listaDeTarefas;
+    }
+  };
+}
