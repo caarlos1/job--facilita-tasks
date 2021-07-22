8ee4cbe4a08878ee97dd22ce086aea76059b9fda
Carlos Roberto <crpu4a@gmail.com>
2021-07-22 15:47:27 -0300

    refactor: modificcação de AcessoFormulario para ter seus proprios Inputs e não importalos

--- src/components/Acesso/AcessoFormulario.vue
+++ src/components/Acesso/AcessoFormulario.vue
@@ -1,18 +1,46 @@
 <template>
   <form action="/tarefas">
     <h3 class="formulario__titulo">Entre com seus dados de acesso.</h3>
-    <util-input descricao="Nome de usuário ou e-mail:" nome="usuario" />
-    <util-input descricao="Senha:" nome="senha" tipo="password" />
-    <util-botao botao="Enviar" :responsivo="true" cor="sucesso--btn" />
+
+    <div class="input__bloco">
+      <label class="input__label" for="'input_usuario' + nome"
+        >Nome de usuário ou e-mail:</label
+      >
+      <input
+        v-model="usuario"
+        id="'input_usuario' + nome"
+        class="input__base input__input"
+        type="text"
+      />
+    </div>
+
+    <div class="input__bloco">
+      <label class="input__label" for="'input_senha' + nome">Senha:</label>
+      <input
+        v-model="senha"
+        id="'input_senha' + nome"
+        class="input__base input__input"
+        type="password"
+      />
+    </div>
+
+    <div class="botao__bloco">
+      <util-botao botao="Enviar" :responsivo="true" cor="sucesso--btn" />
+    </div>
   </form>
 </template>
 
 <script>
 import UtilBotao from "../Utilitarios/UtilBotao.vue";
-import UtilInput from "../Utilitarios/UtilInput.vue";
 
 export default {
-  components: { UtilInput, UtilBotao },
+  components: { UtilBotao },
+  data() {
+    return {
+      usuario: "",
+      senha: "",
+    };
+  },
 };
 </script>
 
