c48db5864c860e457bca9a6b981828d53c043c4c
Carlos Roberto <crpu4a@gmail.com>
2021-07-22 15:36:31 -0300

    refactor: modificação do formato de dados de tarefas
    finalizada agora é concluida e tipo agora possui apenas valor e tag.

--- src/store/modules/ToDoList/ToDoListDados.js
+++ src/store/modules/ToDoList/ToDoListDados.js
@@ -3,30 +3,30 @@ export const tarefas = [
     id: 1,
     titulo: "Criar projeto Vue.js",
     descricao: "Fazer bem feito e bonito.",
-    finalizada: false,
-    importancia: {
-      tipo: 1,
-      texto: "Normal",
+    concluida: false,
+    tipo: {
+      valor: 1,
+      tag: "Normal",
     },
   },
   {
     id: 2,
     titulo: "Começar a estudar Vue.js",
     descricao: "Fazer bem feito e bonito.",
-    finalizada: false,
-    importancia: {
-      tipo: 2,
-      texto: "Importante",
+    concluida: false,
+    tipo: {
+      valor: 2,
+      tag: "Importante",
     },
   },
   {
     id: 3,
     titulo: "Criar projeto Vue.js",
     descricao: "Fazer bem feito e bonito.",
-    finalizada: false,
-    importancia: {
-      tipo: 3,
-      texto: "Urgente",
+    concluida: false,
+    tipo: {
+      valor: 3,
+      tag: "Urgente",
     },
   },
 ];
