7d9dab6f8386f3b81ce8eebff8efa1b69b76cfcf
Carlos Roberto <crpu4a@gmail.com>
2021-07-22 23:19:22 -0300

    feat: componetização de UtilTreeDots para utilização lugares distintos

---
+++ src/components/Utilitarios/UtilTreeDots.vue
@@ -0,0 +1,20 @@
+<template>
+  <i @click="funcao()" class="fas fa-ellipsis-v botao__tree-dots"></i>
+</template>
+
+<script>
+export default {
+  props: {
+    funcao: Function,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.botao__tree-dots
+  padding 0px 0 0 20px
+  align-self center
+  font-size 18px
+  color var( --cor-borda-input )
+  cursor pointer
+</style>
