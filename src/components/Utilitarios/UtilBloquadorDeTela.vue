d0ea986411fc87df1c7e3ae5b16cb0211ad0f17c
Carlos Roberto <crpu4a@gmail.com>
2021-07-22 23:18:40 -0300

    feat: criação de UtilBloqueadorDeTela para desativar menus PopUp ao clicar fora

---
+++ src/components/Utilitarios/UtilBloquadorDeTela.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bloquador__area">
+    <div @click="fechar()" class="bloqueador__tela"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fechar: Function,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.bloqueador__area
+  position relative
+
+.bloqueador__tela
+  z-index 1010
+  position absolute
+  height 100vh
+  width 100vw
+  top 0
+  right 0
+  background black
+  opacity 0.1
+</style>
